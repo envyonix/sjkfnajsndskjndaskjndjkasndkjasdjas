2c7ae7f8fdb82303d6fd15a67bbcabb12aba9152
envyonix <ali.ishtiaq09871@gmail.com>
2025-07-14 18:02:52 +0100

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,14 +8,10 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
+            Hi Everyone, I am known as <span className="purple">Envyonix/ALI  </span>
             from <span className="purple"> Bhubaneswar, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
-            <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
-            <br />
+            I am currently studying for Computer Science at GCSE level.
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
@@ -24,7 +20,7 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Working with minecraft servers
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
